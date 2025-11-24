a1b29a5ffe894272994a75157793a646da0dbbed
Jorjino Garcia <20159238si@gmail.com>
2025-11-24 11:11:00 -0500

    second commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ export const projects = [
     description: 'Sistema HelpDesk con arquitectura de microservicios utilizando Spring Boot y Spring Cloud. Implementé Gateway, Eureka para service discovery y Config Server para gestión centralizada de configuración. Incluye gestión completa de tickets, usuarios y dashboards administrativos con Vue.js en el frontend.',
     tags: ['Spring Boot', 'Spring Security', 'Spring Cloud', 'Microservicios', 'Vue.js', 'MySQL'],
     image: 'https://media.licdn.com/dms/image/v2/D4E2DAQGdmtsg8FyHrg/profile-treasury-image-shrink_800_800/B4EZp0E7O1IQAY-/0/1762884040078?e=1764604800&v=beta&t=P8gOAs7Q_1vK-wVQ-lAqm00zUlMqGdNdgmufeK6uJxQ',
-    url: 'https://github.com/kalebjorjino',
+    url: 'https://github.com/kalebjorjino/Heldesk-microservices-java-spring-boot.git/',
     demo: '#'
   },
   {
@@ -15,8 +15,8 @@ export const projects = [
     description: 'Aplicación web para registrar y controlar el ingreso y salida de visitantes en el Hospital de Rehabilitación del Callao. Desarrollada con PHP, MySQL y Bootstrap. Incluye reportes de visitas, dashboards de estadísticas y gestión de accesos. Sistema en producción desde mayo 2024.',
     tags: ['PHP', 'MySQL', 'Bootstrap', 'HTML5', 'CSS3', 'JavaScript'],
     image: 'https://media.licdn.com/dms/image/v2/D4E2DAQHwoVlhqR78dQ/profile-treasury-image-shrink_800_800/B4EZpz4Y3cGcAc-/0/1762880753829?e=1764604800&v=beta&t=ucPuSyQGyxNoCSwcjVJonrt1_WgIBKjO3j3HP4R0-q4',
-    url: 'https://github.com/kalebjorjino',
-    demo: '#'
+    url: 'https://github.com/kalebjorjino/asistencia-frontend.git/',
+    demo: 'https://www.hrcallao.gob.pe/visitas/'
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ export const projects = [
     description: 'Aplicación web para registrar y controlar el ingreso y salida de visitantes en el Hospital de Rehabilitación del Callao. Desarrollada con PHP, MySQL y Bootstrap. Incluye reportes de visitas, dashboards de estadísticas y gestión de accesos. Sistema en producción desde mayo 2024.',
     tags: ['PHP', 'MySQL', 'Vue.js', 'HTML5', 'CSS3', 'JavaScript'],
     image: 'https://media.licdn.com/dms/image/v2/D4E2DAQEYY_uZ2agvVg/profile-treasury-image-shrink_800_800/B4EZpzopYsKkAY-/0/1762876627171?e=1764604800&v=beta&t=C_hGA5SUjcWKeKGzDXKFBP-qqR-twdesxQDFoDR3-hI',
-    url: 'https://github.com/kalebjorjino',
+    url: 'https://github.com/kalebjorjino/asistencia-api.git/',
     demo: '#'
   },
 ]
